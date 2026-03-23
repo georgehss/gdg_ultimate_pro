00420aa188f030b616965e7ed77ec01f621f19aa
georgehss <georgeharrisonss@hotmail.com>
2026-03-23 14:50:27 -0300

    Update 23/03/2026

--- gh_hiove_bot/src/scraper/hiove_scraper.py
+++ gh_hiove_bot/src/scraper/hiove_scraper.py
@@ -361,11 +361,11 @@ class HioveScraper:
             # ==========================================
             logger.info(f"⏳ [{symbol}] Operação finalizada. Aguardando a corretora actualizar o histórico...")
             # Tempo drasticamente reduzido para agilizar o Martingale
-            await asyncio.sleep(2.0)
+            await asyncio.sleep(1.0)
             # 1. Clica na aba de 'Histórico' para garantir que as ordens fechadas aparecem
             xpath_btn_historico = '//*[@id="sider-trade"]/div/div/div/div[1]/button[2]'
             await page.locator(f'xpath={xpath_btn_historico}').click(timeout=5000)
-            await asyncio.sleep(2.0) # Dá um tempinho para a lista carregar
+            await asyncio.sleep(2.5) # Dá um tempinho para a lista carregar
             
             # 2. Localiza os itens e FILTRA pelo ativo específico daquela ordem
             xpath_itens = '//*[@id="sider-trade"]/div/div/div/div[2]//li'
